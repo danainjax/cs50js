9a0b360aa86516d9f3b3ef6aaebecdf892247506
danainjax <danainjax@gmail.com>
2021-07-15 12:33:49 -0400

    first commit

---
+++ counter.js
@@ -0,0 +1,22 @@
+const h1 = document.getElementById('h1');
+const bttn = document.getElementById('bttn')
+const wordSpelled = document.querySelector("#spelling")
+const speller = document.querySelector("#spell")
+const spellBttn = document.querySelector("#spell-button")
+let counter = 0;
+
+function count() {
+    counter ++;
+    h1.innerHTML = counter;
+    alert(`You are counting to ${counter}`)
+}
+
+bttn.addEventListener('click', count)
+
+spellBttn.addEventListener('click', spell)
+
+function spell() {
+    console.log(speller.value)
+    wordSpelled.innerHTML = speller.value ;
+    speller.value = " "
+}
