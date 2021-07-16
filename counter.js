25b8174b61bab03e4c91c40a17a380091f53cee7
danainjax <danainjax@gmail.com>
2021-07-16 14:47:02 -0400

    started learning about local storage

--- counter.js
+++ counter.js
@@ -3,14 +3,26 @@ const bttn = document.getElementById('bttn')
 const wordSpelled = document.querySelector("#spelling")
 const speller = document.querySelector("#spell")
 const spellBttn = document.querySelector("#spell-button")
-let counter = 0;
+// let counter = 0;
+document.addEventListener('DOMContentLoaded', () => {
+    
+    h1.innerHTML = localStorage.getItem('counter')
+
+})
 
 function count() {
+    let bicycle = localStorage.getItem('bicycle')
+    let counter = localStorage.getItem('counter');
     counter ++;
+    bicycle ++
     h1.innerHTML = counter;
+    localStorage.setItem('counter', counter);
+    localStorage.setItem('bicycle', bicycle)
+    console.log(localStorage.getItem('bicycle'))
 }
 
-setInterval(count, 1000)
+
+// setInterval(count, 1000)
 
 bttn.addEventListener('click', count)
 
