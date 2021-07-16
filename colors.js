e80204a572b0f431b59892bee89210ac7881ac6d
danainjax <danainjax@gmail.com>
2021-07-16 08:33:53 -0400

    dataset property and adding event listener to a nodelist

--- colors.js
+++ colors.js
@@ -2,15 +2,22 @@ const red = document.getElementById('red');
 const blue = document.getElementById('blue');
 const green = document.getElementById('green');
 const hello = document.querySelector('#hello');
+const buttons = document.querySelectorAll('button');
 
-red.addEventListener('click', function () {
-    document.querySelector('#hello').style.color = 'red';
-})
+// red.addEventListener('click', function () {
+//     document.querySelector('#hello').style.color = 'red';
+// })
 
-blue.addEventListener('click', function() {
-    hello.style.color = 'blue';
-})
+// blue.addEventListener('click', function() {
+//     hello.style.color = 'blue';
+// })
 
-green.addEventListener('click', function() {
-    hello.style.color = 'green';
+// green.addEventListener('click', function() {
+//     hello.style.color = 'green';
+// })
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        hello.style.color = button.dataset.color;
+    })
 })
