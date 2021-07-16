fb88fc92947d2f700c401771758b953a1228ce7d
danainjax <danainjax@gmail.com>
2021-07-16 09:56:17 -0400

    added condional logic to display of submit button

--- tasks.js
+++ tasks.js
@@ -14,5 +14,10 @@ form.addEventListener('submit', (event) => {
 document.querySelector('#submit').disabled = true;
 
 document.querySelector('#task').addEventListener('keyup', () => {
-    document.querySelector('#submit').disabled = false;
+    if (document.querySelector('#task').value.length > 0) {
+        document.querySelector('#submit').disabled = false;
+    } else {
+        document.querySelector('#submit').disabled = true;
+    }
+
 })
